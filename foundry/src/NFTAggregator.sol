24f45bd43afe148b87fe125f7c2329047069fd7e
Vector <vector02ozo@gmail.com>
2025-04-20 17:32:20 +0100

    header commit

--- foundry/src/NFTAggregator.sol
+++ foundry/src/NFTAggregator.sol
@@ -2,4 +2,9 @@
 
 pragma solidity ^0.8.18;
 
-contract NFTAggregator {}
+contract NFTAggregator {
+
+    constructor {
+
+    }
+}
---
+++ nextjs/app/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RxStitchesLogo } from "react-icons/rx";
+
+export const Header = () => {
+  return (
+    <header className="flex justify-between items-center gap-4">
+      <div className="flex justify-between items-center gap-2">
+        <RxStitchesLogo size={40} />
+        <div>Nerfed</div>
+      </div>
+      {/* <div></div> */}
+      <button>Connected</button>
+    </header>
+  )
+}
