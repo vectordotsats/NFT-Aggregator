399fe938d9d9edf4fa0df6c6d5959948da772b9d
Vector <vector02ozo@gmail.com>
2025-04-26 20:09:58 +0100

    body commit

--- nextjs/app/components/Body.jsx
+++ nextjs/app/components/Body.jsx
@@ -1,12 +1,70 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 export const Body = () => {
   return (
     <div className='mt-4 mb-4 p-4 bg-white rounded-lg shadow-md'>
+
+      {/* First Nft */}
         <div>
-            <img src="../img/No10.jpeg" alt="" />
-            <div> This is working</div>
+            <Image src="/nextjs/public/No1.svg" alt="" width={100} height={80}></Image>
+            <div>
+              <div>
+                <div>Name of Art</div>
+                <div>Price: 0.1ETH</div>
+              </div>
+              <div>Marketplace</div>
+            </div>
         </div>
+
+        {/* Second Nft */}
+        <div>
+            <Image src="/nextjs/public/No1.svg" alt="" width={100} height={80}></Image>
+            <div>
+              <div>
+                <div>Name of Art</div>
+                <div>Price: 0.1ETH</div>
+              </div>
+              <div>Marketplace</div>
+            </div>
+        </div>
+
+        {/* Third Nft */}
+        <div>
+            <Image src="/nextjs/public/No1.svg" alt="" width={100} height={80}></Image>
+            <div>
+              <div>
+                <div>Name of Art</div>
+                <div>Price: 0.1ETH</div>
+              </div>
+              <div>Marketplace</div>
+            </div>
+        </div>
+
+        {/* Fourth Nft */}
+        <div>
+            <Image src="/nextjs/public/No1.svg" alt="" width={100} height={80}></Image>
+            <div>
+              <div>
+                <div>Name of Art</div>
+                <div>Price: 0.1ETH</div>
+              </div>
+              <div>Marketplace</div>
+            </div>
+        </div>
+
+        {/* Fifth Nft */}
+        <div>
+            <Image src="/nextjs/public/No1.svg" alt="" width={100} height={80}></Image>
+            <div>
+              <div>
+                <div>Name of Art</div>
+                <div>Price: 0.1ETH</div>
+              </div>
+              <div>Marketplace</div>
+            </div>
+        </div>
+        
     </div>
   )
 }
