19606a75f7ef58f759e8e85b4fbca888dfd95637
Vector <vector02ozo@gmail.com>
2025-05-05 18:38:36 +0100

    dashboard commit

--- nextjs/app/page.jsx
+++ nextjs/app/page.jsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 import {Header} from "./components/Header.jsx";
 import {Body} from "./components/Body.jsx";
+import { Dashboard } from "./components/Dashboard.jsx";
 
 export default function Home() {
   return (
     <div className="w-[95%] m-auto font-[family-name:var(--font-geist-sans)]">
       <Header />
+      <Dashboard />
       <Body />
     </div>
   );
